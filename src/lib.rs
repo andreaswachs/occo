b8fcb9a3beb70bf6aa16c24cc868a6dbf0d092fc
Andreas Wachs <andertraaks@gmail.com>
2021-03-29 23:02:36 +0200

    Reach MVP milestone

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,38 @@
+use std::fs;
+use std::process;
+
 pub mod input {
 
-    pub fn read_file(filename: &str) {
-        println!("{}", format!("Im reading: {}", filename));
-        
+    fn read_file(filename: &str) -> Result<String, &'static str> {
+        match std::fs::read_to_string(filename) {
+            Ok(s) => Ok(s),
+            Err(e) => return Err("Shit!"),
+        }
+    }
 
+    pub fn find_occurrences(word: &str, filename: &str) {
+        let contents = read_file(filename)
+            .expect("Something went wrong with reading the file");
+        
+        if contents.trim().is_empty() {
+            println!("The file was empty!");
+        }
+
+        let mut count: i32 = 0;
+        let mut modifier = String::new();
+
+        for line in contents.lines() {
+            if line.contains(&word) {
+                count += 1;
+            }
+        }
+
+        if count > 1 {
+            modifier = String::from("s");
+        }
+
+        println!("The world {} occurred {} time{} in the text file.",
+            word, count, modifier);
     }
 }
 
